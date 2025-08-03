18aa865633a29e28ed9391568bc0cfe4f32e60eb
veshutov <veshutov98@gmail.com>
2025-08-03 17:45:05 +0300

    migrations

---
+++ lib/db/migrations/0000_skinny_unus.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "guess" (
+	"user_id" varchar NOT NULL,
+	"word" varchar NOT NULL,
+	"date" date NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "guess_user_id_date_word_pk" PRIMARY KEY("user_id","date","word")
+);
+--> statement-breakpoint
+CREATE TABLE "word" (
+	"word" varchar NOT NULL,
+	"rank" integer NOT NULL,
+	"date" date NOT NULL,
+	CONSTRAINT "word_word_date_pk" PRIMARY KEY("word","date")
+);
